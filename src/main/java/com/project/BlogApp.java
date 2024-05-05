e351790022a55caaa389bdfe8672804a434e9265
Dmitry_Bovkun <dimon4ikbovkun@gmail.com>
2024-05-05 20:42:01 +0300

    DAST

--- src/main/java/com/project/BlogApp.java
+++ src/main/java/com/project/BlogApp.java
@@ -4,7 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @EnableScheduling
@@ -17,4 +24,15 @@ public class BlogApp extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(BlogApp.class);
 	}
+
+	@ControllerAdvice
+	public static class GlobalExceptionHandler {
+		@ExceptionHandler(Exception.class)
+		public ResponseEntity<Map<String, String>> handleException(Exception ex) {
+			Map<String, String> errorResponse = new HashMap<>();
+			errorResponse.put("error", "Internal server error. Please try again later.");
+			return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
 }
